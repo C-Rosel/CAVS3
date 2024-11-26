66d85b9d3ae909ccfc64edf615c50aa007c2210e
CanonSb <canon.sbravati@yahoo.com>
2024-11-25 22:44:35 -0600

    Add tabs to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,16 @@
 // src/components/navbar/Sidebar.jsx
 
 import React, {useState} from 'react'
+import { IoMapSharp, IoCameraSharp, IoRadioSharp, IoBarChartSharp  } from "react-icons/io5";
 import './Sidebar.css'
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
     return (
         <nav className={`${sidebarOpen ? "open" : ""} sidebar`}>
-            <div className='sidebarBtn'></div>
-            <div className='sidebarBtn '></div>
-            <div className='sidebarBtn '></div>
+            <div className='sidebarBtn'><IoMapSharp size={50}/>GPS</div>
+            <div className='sidebarBtn'><IoCameraSharp size={50}/>Camera</div>
+            <div className='sidebarBtn'><IoRadioSharp size={50}/>Sensors</div>
+            <div className='sidebarBtn'><IoBarChartSharp size={50}/>Data</div>
         </nav>
     );
 };
